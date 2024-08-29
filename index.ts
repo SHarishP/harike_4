0df0a18a08c1d44cbbacb3520b46e66860e55e4e
SHarishP <setiawanharish85@gmail.com>
2024-08-29 21:28:27 +0700

    refactor: improved index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
+// Latihan menggunakan git dan GitHub
 const _number:number =5;
+const _string:string = "Hallo"
+// Setelah melakukan git commit, akan terdeteksi perubahan pada file. 
+// Maka perlu dilakukan git add kembali.
